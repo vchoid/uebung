d1fe1764f0a9bfb7a47ce7d11811ece11eb107cd
Christoph Kiank <christoph.kiank@bitmarck.de>
2017-09-21 15:34:08 +0200

    ..

---
+++ WebContent/JS/eventhandler.js
@@ -0,0 +1,9 @@
+// Formel ->  kg/(m*m)
+"use strict";
+let height = document.getElementById('height');
+let weight = document.getElementById('weight');
+
+height.addEventListener("change", function() {
+
+
+})
--- WebContent/JS/objektÜbung.js
+++ WebContent/JS/objektÜbung.js
@@ -1,5 +1,54 @@
-function ship(sName, sModel){
-	this.name = sName;
-	this.model = sModel;
+function ship(shipName, shipModel, kaptain){
+	this.name = shipName;
+	this.model = shipModel;
+	this.distance = 0;
+	this.move = function moveFromEarthAway(lightyears){
+		this.distance += lightyears;
+		with(this){
+			document.write(name + ' => act. Journey:' + lightyears + ' lightyears -> earth distance: ' + distance + ' lightyears <br>');
+		}
+	this.back = function backToEarth(ly){
+		with(this){
+			distance -= ly;
+			if(distance > 0){
+				document.write(name + ' is coming back to earth: just ' + distance + ' lightyears <br>' );
+			} else if(distance == 0){
+				document.write('Welcome home ' + name + '<br>');
+			} else {
+				document.write(name + ' is already there <br>');
+			}
+		}
+	}
+	}
 }
 
+var enterprise = new ship('U.S.S. Enterprise', 'NCC-1701');
+var voyager = new ship('U.S.S. Voyager', 'NCC-74656');
+
+document.write('<p> Both spaceships starts from earth: </p>');
+enterprise.move(50);
+voyager.move(180);
+
+enterprise.move(200);
+voyager.move(-50);
+
+enterprise.move(-100);
+enterprise.move(-150);
+
+voyager.move(40);
+voyager.move(-170);
+
+for(var laufVar in enterprise){
+	document.write('Bezeichnung: ' + laufVar + ' -> Wert: ' + enterprise[laufVar] + '<br>');
+}
+
+document.write('move' in enterprise);
+document.write('<br>');
+document.write(enterprise instanceof ship);
+document.write('<br>');
+document.write('<p>' + enterprise.name + ' starts from earth: </p>');
+enterprise.move(50);
+enterprise.move(100);
+enterprise.back(50);
+enterprise.back(100);
+enterprise.back(50);
