5bd2f2af38196eb6cc4ebe0ac8a88a63ffaf010f
Christoph Kiank <christoph.kiank@bitmarck.de>
2017-09-25 11:40:16 +0200

    neue Aufgabe und alten Code umgeschrieben

--- WebContent/JS/objekt.js
+++ WebContent/JS/objekt.js
@@ -1,6 +1,7 @@
 'use strict';
-class BankAcc {
 
+
+class BankAcc {
     constructor(balance) {
         this._initBalance = balance;
         this._withdraws = [];
@@ -29,7 +30,35 @@ class BankAcc {
     }
 }
 
+/* *************************************** alte Schreibweise
+function BankAcc(balance) {
+    this._initBalance = balance;
+    this._withdraws = [];
+}
+
+BankAcc.prototype.withdraw = function(amount) {
+    let withD = this._withdraws;
+    if (this.getBalance() - amount <= 0) {
+        document.getElementById('info').innerHTML = 'Nicht genug Geld um ' + amount + ' abzuheben.';
+    } else {
+        withD.push(amount);
+    }
+}
+BankAcc.prototype.getBalance = function() {
+    let balance = this._initBalance;
+    for (let wd of this._withdraws) {
+        balance = balance - wd;
+    }
+    return balance;
+}
+
+BankAcc.prototype.showBalance = function() {
+    let bal = document.getElementById('balance');
+    return bal.innerHTML = this.getBalance();
+}
+*/
 let a = new BankAcc(1000);
+console.log(a);
 a.withdraw(200);
 a.withdraw(500);
 a.withdraw(800);
